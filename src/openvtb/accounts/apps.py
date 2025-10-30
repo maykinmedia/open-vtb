0155aba0b623bf5ab7c8fc971607be164aded409
Daniel Mursa <daniel@maykinmedia.nl>
2025-10-30 12:28:21 +0100

    :wrench: Add default_admin_index fixtures

--- src/openvtb/accounts/apps.py
+++ src/openvtb/accounts/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class AccountsConfig(AppConfig):
     name = "openvtb.accounts"
+
+    def ready(self):
+        from . import signals  # noqa
+
+        post_migrate.connect(signals.update_admin_index, sender=self)
