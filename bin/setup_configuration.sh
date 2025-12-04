51e337b1be27983f688039c064e9898696ab9229
Daniel Mursa <daniel@maykinmedia.nl>
2025-12-04 15:59:56 +0100

    :heavy_plus_sign: [#14] Add django-setup-configuration

---
+++ bin/setup_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# setup initial configuration using environment variables
+# Run this script from the root of the repository
+
+set -e
+
+# Set defaults for OTEL
+export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-openvtb-setup-configuration}"
+
+
+if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
+    # wait for required services
+    /wait_for_db.sh
+
+    OTEL_SDK_DISABLED=True src/manage.py migrate
+    OTEL_SDK_DISABLED=True src/manage.py setup_configuration --yaml-file setup_configuration/data.yaml
+fi
