00b133fb124df524bdc86b754d2f81e8bf4b7112
Daniel Mursa <daniel@maykinmedia.nl>
2026-03-25 11:14:25 +0100

    :white_check_mark: Add extra tests for accounts app

--- src/openvtb/accounts/tests/test_createinitialsuperuser.py
+++ src/openvtb/accounts/tests/test_createinitialsuperuser.py
@@ -5,6 +5,8 @@ from django.core import mail
 from django.core.management import call_command
 from django.test import TestCase, override_settings
 
+from openvtb.accounts.tests.factories import UserFactory
+
 from ..models import User
 
 
@@ -60,3 +62,24 @@ class CreateInitialSuperuserTests(TestCase):
             f"Your admin user for {settings.PROJECT_NAME} (unknown url)",
         )
         self.assertEqual(sent_mail.recipients(), ["support@maykinmedia.nl"])
+
+    @override_settings(ALLOWED_HOSTS=["*"])  # test all
+    def test_create_superuser_already_exists(self):
+        self.assertEqual(User.objects.count(), 0)
+        UserFactory.create(superuser=True, username="maykin")
+
+        self.assertEqual(User.objects.count(), 1)
+
+        call_command(
+            "createinitialsuperuser",
+            "--generate-password",
+            "--email-password-reset",
+            username="maykin",
+            email="support@maykinmedia.nl",
+            stdout=StringIO(),
+        )
+
+        self.assertEqual(User.objects.count(), 1)
+
+        user = User.objects.get()
+        self.assertEqual(user.username, "maykin")
